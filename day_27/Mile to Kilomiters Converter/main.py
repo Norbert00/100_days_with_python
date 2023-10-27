cc62ba6f7ebc9b9c4efe0bee221d8f6391e7f40b
Norbert00 <norbert.jurek@protonmail.com>
2023-10-27 16:59:54 +0200

    Added field to display converted miles to kilometres

--- day_27/Mile to Kilomiters Converter/main.py
+++ day_27/Mile to Kilomiters Converter/main.py
@@ -8,8 +8,14 @@ window = Window(width=500, height=300)
 equal_label = Labels(column=0, row=3, text="Is equal to", font="Arial", size=10, style="italic")
 miles_label = Labels(column=2, row=1, text="Miles", font="Arial", size=10, style="bold")
 km_label = Labels(column=2, row=3, text="Km", font="Arial", size=10, style="bold")
-calculate = Buttons(column=1, row=4)
+
 user_input = User_Input(column=1, row=1)
+
+calculate = Buttons(column=1, row=4)
+calculate.bind_click_event(user_input.set_data)
+
+
+converted_kilometer_label = Labels(column=1, row=3, text=f"{user_input.kilometer}", font="Arial", size=10, style="bold")
 
 
 window.mainloop()
