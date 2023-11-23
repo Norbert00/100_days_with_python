d9bf53d2ca010ae867e933b680ad788bde4ccc2d
Norbert00 <norbert.jurek@protonmail.com>
2023-11-23 11:10:18 +0100

    Fixed a problem with the display of values for kilometres when the calculate button is pressed

--- day_27/Mile to Kilomiters Converter/main.py
+++ day_27/Mile to Kilomiters Converter/main.py
@@ -1,7 +1,7 @@
 from window import Window
 from button import Buttons
 from text_labels import Labels
-from user_input import User_Input
+from user_input import UserInput
 
 window = Window(width=500, height=300)
 
@@ -9,13 +9,12 @@ equal_label = Labels(column=0, row=3, text="Is equal to", font="Arial", size=10,
 miles_label = Labels(column=2, row=1, text="Miles", font="Arial", size=10, style="bold")
 km_label = Labels(column=2, row=3, text="Km", font="Arial", size=10, style="bold")
 
-user_input = User_Input(column=1, row=1)
+user_input = UserInput(column=1, row=1)
 
 calculate = Buttons(column=1, row=4)
-calculate.bind_click_event(user_input.set_data)
-
+calculate.bind_click_event(
+    lambda: (user_input.set_data(), user_input.update_converted_label(converted_kilometer_label)))
 
 converted_kilometer_label = Labels(column=1, row=3, text=f"{user_input.kilometer}", font="Arial", size=10, style="bold")
-
 
 window.mainloop()
